2df03cf6687fcf5d2571f633a8a27fb37103662f
Stefan Zabrian <faniss2005@yahoo.com>
2023-03-09 20:46:59 +0200

    PTG-1 Print periodically current Date and Time on 5s intervals

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,23 @@
 package org.example;
 
+import java.time.Instant;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+
+        printPeriodicallyCurrentDateAndTime();
+    }
+
+    private static void printPeriodicallyCurrentDateAndTime() {
+        while (true) {
+            try {
+                System.out.println(Instant.now());
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
